f0bd914bbd8f32e1dd0b8d89ab35190c3c3b44bc
daqiaomay-lgtm <daqiaomay@gmail.com>
2025-12-15 16:44:44 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,23 @@
 export default {
   async scheduled(event, env, ctx) {
-    console.log("cron fired");
+    const masterUrl = `${env.KINTONE_BASE_URL}/k/v1/records.json?app=${env.KINTONE_MASTER_APP_ID}`;
+    const stockUrl  = `${env.KINTONE_BASE_URL}/k/v1/records.json?app=${env.KINTONE_STOCK_APP_ID}`;
+
+    const [masterRes, stockRes] = await Promise.all([
+      fetch(masterUrl, { headers: { "X-Cybozu-API-Token": env.KINTONE_MASTER_API_TOKEN } }),
+      fetch(stockUrl,  { headers: { "X-Cybozu-API-Token": env.KINTONE_STOCK_API_TOKEN } }),
+    ]);
+
+    if (!masterRes.ok) console.error("master error", masterRes.status, await masterRes.text());
+    if (!stockRes.ok)  console.error("stock error",  stockRes.status,  await stockRes.text());
+
+    const master = masterRes.ok ? await masterRes.json() : null;
+    const stock  = stockRes.ok  ? await stockRes.json()  : null;
+
+    console.log("master records:", master?.records?.length ?? "NG");
+    console.log("stock records:",  stock?.records?.length ?? "NG");
   }
 };
+
+
 
